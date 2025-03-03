8ada791380607b00d60cc8970f2ea4f7fa225c68
noambezalel12345 <noambezalel2008@gmail.com>
2025-03-03 18:13:27 +0200

    complite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation ("ccom.google.android.gms:play-services-auth:21.3.0")
 
 }
